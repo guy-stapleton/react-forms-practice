1c8b4be4c2c1304b027ac6dccff86bfac0edd284
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-02 08:04:06 +1300

    Set up basic form linking

---
+++ client/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Form extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  // Methods
+  render() {
+    return <form>
+      <h1>1990s Basketball Players</h1>
+    </form>
+  }
+}
+
+export default Form

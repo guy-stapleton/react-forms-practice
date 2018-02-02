b8543c5f60777dbfde17ac3e4f41c79af4eefee7
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-02-02 20:19:31 +1300

    Set up form to add to table

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -3,15 +3,45 @@ import React from 'react'
 
 import Form from './Form'
 
-const App = () => {
-  return (
-    <div>
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      players: [
+        {name: 'Michael Jordan', position: 'Shooting Guard'}
+      ]
+    }
+
+    this.addPlayer = this.addPlayer.bind(this)
+  }
+  //
+  addPlayer(player) {
+    const {players} = this.state
+    players.push(player)
+    this.setState({players})
+  }
+  render() {
+    return <div>
       <h1>React development has begun!</h1>
-      <Form />
+      <Form add={this.addPlayer}/>
+      <table>
+  <thead>
+   <tr>
+   <td>Name</td>
+   <td>Position</td>
+   </tr>
+  </thead>
+<tbody>
+   {this.state.players.map((player, i) =><tr key={i}>
+ <td>{player.name}</td>
+ <td>{player.position}</td>
+</tr>)}
+</tbody>
+</table>
+</div>
+  }
 
-    </div>
-
-  )
 }
 
 export default App
--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -3,13 +3,39 @@ import React from 'react'
 class Form extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      newPlayer: {
+        name: '',
+        position: ''
+      }
+    }
+    this.updateField = this.updateField.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
   }
   // Methods
+  updateField(e) {
+    console.log(e.target.name, e.target.value)
+    const {newPlayer} = this.state
+    newPlayer[e.target.name] = e.target.value
+    this.setState({newPlayer})
+  }
+
+  onSubmit(e) {
+    e.preventDefault()
+    this.props.add(this.state.newPlayer)
+    this.setState({newPlayer: {
+      name: '',
+      position: ''
+    }})
+    console.log(this.state)
+  }
+  // Render
   render() {
-    return <form>
+    const {name, position} = this.state.newPlayer
+    return <form onSubmit={this.onSubmit}>
       <h1>1990s Basketball Players</h1>
-      <input placeholder="name" name="name" type="text" />
-      <input placeholder="position" name="name" type="text" />
+      <input placeholder="name" name="name" type="text" onChange={this.updateField} />
+      <input placeholder="position" name="position" type="text" onChange={this.updateField}/>
       <input type="submit" value="Add a player" />
     </form>
   }
